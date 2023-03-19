3ad9a1aa9f1c7f875629ee9a0dae3cf4464fb80c
romgnom <126273888+RomanPuryskin@users.noreply.github.com>
2023-03-19 10:24:16 +0000

    Реализованы методы -получение вектора ключей -вывод листьев дерева -вывод дерева

--- .ccls-cache/@home@runner@Binarnoie-dierievo/main.cpp
+++ .ccls-cache/@home@runner@Binarnoie-dierievo/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 class Node
 {
@@ -8,6 +10,9 @@ private:
   Node* m_right;
 
 public:
+
+  ~Node() = default;
+
   Node()
   {
     m_key=0;
@@ -90,6 +95,24 @@ private:
 //------------------------------------//
 
 
+//---------------Удаление дерева (поддерева)-------------------//
+
+  void deleteSubTree(Node *root)
+  {
+    if(root)
+      {
+        deleteSubTree(root->GetLeft());
+        deleteSubTree(root->GetRight());
+        root = NULL;
+      }
+  }
+
+  void deleteAllTree()
+  {
+    deleteSubTree(m_root);
+  }
+//-------------------------------------------------------------//
+
 //----------------Конструктор копирования-----------------//
   BinaryTree(const BinaryTree& copy)
   {
@@ -100,8 +123,19 @@ private:
   {
     ;
   }
+//-------------------------------------------------------//
 
-//------------Добавление элемента ( перегрузка ) ------------------//
+//------------------Проверка дерева на пустоту------------------//
+bool isEmpty(Node *root)
+  {
+    if (root == NULL)
+      return true;
+    return false;
+  }
+//--------------------------------------------------------------//
+
+
+//------------Добавление элемента ( перегрузка ) ------//
   Node* addNode(Node *root , int key)
   {
     if(!root)
@@ -120,7 +154,7 @@ private:
     m_root=addNode(m_root ,key);
     return m_root;
   }
-//----------------------------------------------------------------//
+//------------------------------------------------------//
 
 //------------Получение корня-------------------//
   Node* getRoot()
@@ -128,19 +162,79 @@ private:
     return m_root;
   }
 //----------------------------------------------//
-  
-};
 
-void destr(const int size)
-{
-  BinaryTree tree;
-  for(int i=0;i<size;i++)
-    tree.addNode(i);
-}
+  int Count(Node *root)
+  {
+    if(root == NULL)
+      return 0;
+    else
+    {
+      if(root->GetLeft()!=NULL)
+        return Count(root->GetLeft())+1;
+      if(root->GetRight()!=NULL)
+        return Count(root->GetRight())+1;
+    }
+  }
+
+//----------------------Вывод дерева горизонтально-----------------//
+  void PrintTree(Node *root, int marginLeft, int Space) const
+  {
+    if(!root)
+      return;
+    PrintTree(root->GetRight(),marginLeft+Space,Space);
+    std::cout<<std::string(marginLeft,' ')<<root->GetKey()<<std::endl;
+    PrintTree(root->GetLeft(),marginLeft+Space,Space);
+    
+  }
+//-----------------------------------------------------------------//
+
+//------------------Вывод листьев дерева-----------------//
+  void PrintLeaves(Node *root) const
+  {
+    if(!root)
+      return;
+    if(root)
+    {
+      PrintLeaves(root->GetLeft());
+      PrintLeaves(root->GetRight());
+      if(root->GetLeft() == NULL && root->GetRight() == NULL)
+        std::cout<<root->GetKey()<<" ";
+    }
+  }
+//-------------------------------------------------------//
+
+//---------------Получение вектора ключей--------------//
+void getTreeKeys(Node *root) const
+  {
+    std::vector<int> treeKeys;
+    if(!root)
+      return;
+    if(root)
+    {
+      getTreeKeys(root->GetLeft());
+      getTreeKeys(root->GetRight());
+      treeKeys.push_back(root->GetKey());
+    }
+
+    for (auto& v : treeKeys){
+        std::cout << v << " ";
+    }
+  }
+//-----------------------------------------------------//
+};
 
 
 int main()
 {
-  destr(100);
+  int pup;
+  BinaryTree tree;
+  for(int i=0;i<19;i++)
+    tree.addNode(i);
+
+  tree.PrintTree(tree.getRoot(),2 ,3);
+ // pup = tree.Count(tree.getRoot());
+  //std::cout<<pup;
+  //tree.PrintLeaves(tree.getRoot());
+
 return 0;
   }
